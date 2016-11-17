1f7f06a60e8499106ff5b35c8f9c68c2da548b59
Ally Cody <allysoncody2015@u.northwestern.edu>
2016-11-17 11:47:57 -0500

    fixed cannot read propery push of undefined stack.push

--- twilio/api.js
+++ twilio/api.js
@@ -1,16 +1,16 @@
-var twilioAPI = require('express').Router
+var twilioAPI = require('express').Router()
 // var twilio = require('twilio')
 
 twilioAPI.get('/')
 
 twilioAPI.post('/twilio/messages', function(req, res, next){
   console.log("Hey this is a message")
-  // var twiml = new twilio.TwimlResponse();
-  //   twiml.message(function() {
-  //     this.body('The Robots are coming! Head for the hills!');
-  //   });
-  //   res.writeHead(200, {'Content-Type': 'text/xml'})
-  //   res.end(twiml.toString())
+  var twiml = new twilio.TwimlResponse();
+    twiml.message(function() {
+      this.body('The Robots are coming! Head for the hills!');
+    });
+    res.writeHead(200, {'Content-Type': 'text/xml'})
+    res.end(twiml.toString())
 });
 
 module.exports = twilioAPI
