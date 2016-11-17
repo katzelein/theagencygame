236840c526cb9edd015f007773fc761c77f3c801
Karin Kuo <evnissyn@gmail.com>
2016-11-17 18:45:15 -0500

    general stuff

--- twilio/api.js
+++ twilio/api.js
@@ -1,16 +1,20 @@
 var twilioAPI = require('express').Router()
 var twilio = require('twilio')
 
+var lookup = require('./lookup')
+
 twilioAPI.get('/')
 
 twilioAPI.post('/messages', function(req, res, next){
-  console.log("Hey this is a message")
-  var twiml = new twilio.TwimlResponse();
-    twiml.message(function() {
-      this.body('The Robots are coming! Head for the hills!');
-    });
-    res.writeHead(200, {'Content-Type': 'text/xml'})
-    res.end(twiml.toString())
+	console.log(req.body.Body)
+
+	var answer = lookup(req.body.From, req.body.Body);
+	var twiml = new twilio.TwimlResponse();
+	twiml.message(function() {
+		this.body('The Robots are coming! Head for the hills!');
+	});
+	res.writeHead(200, {'Content-Type': 'text/xml'})
+	res.end(twiml.toString())
 });
 
 module.exports = twilioAPI
---
+++ twilio/lookup.js
@@ -0,0 +1,14 @@
+module.exports = function(userNum, userInput) {
+	// lookup userNum in userStatus table for currentState
+	switch (userInput) {
+		case 'hiatus':
+		case 
+	}
+	let currentState = {};
+	// lookup currentState and userInput in stateTransition table for nextState
+	let nextState = {};
+	// if lookup for nextState fails, return "sorry, the Agency's automated text processing has epic failed"
+	// lookup nextState in stateMessage table for message
+	let message = {};
+	return message;
+}
