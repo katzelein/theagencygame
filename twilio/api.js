4a668e02d821f96535eac9dcb190e593e53348d9
Kat Guthrie <kathrynguthrie@gmail.com>
2016-11-17 10:42:55 -0500

    small twilio route change

--- twilio/api.js
+++ twilio/api.js
@@ -2,12 +2,13 @@ var twilioAPI = require('express').Router
 var twilio = require('twilio')
 
 twilioAPI.post('/twilio/messages', function(req, res, next){
-  var twiml = new twilio.TwimlResponse();
-    twiml.message(function() {
-      this.body('The Robots are coming! Head for the hills!');
-    });
-    res.writeHead(200, {'Content-Type': 'text/xml'})
-    res.end(twiml.toString())
+  console.log("Hey this is a message")
+  // var twiml = new twilio.TwimlResponse();
+  //   twiml.message(function() {
+  //     this.body('The Robots are coming! Head for the hills!');
+  //   });
+  //   res.writeHead(200, {'Content-Type': 'text/xml'})
+  //   res.end(twiml.toString())
 });
 
 module.exports = twilioAPI
