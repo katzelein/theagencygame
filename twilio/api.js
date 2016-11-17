f09c89e8e5239fe96d69aee559fe92807ec8dbaa
Ally Cody <allysoncody2015@u.northwestern.edu>
2016-11-17 16:34:45 -0500

    added delete.js to clear out all messages

--- twilio/api.js
+++ twilio/api.js
@@ -3,16 +3,16 @@ var twilio = require('twilio')
 
 twilioAPI.get('/')
 
-twilioAPI.post('/messages', function(req, res, next){
+twilioAPI.post('/messages', function(req, res, next){ 
   //console.log("Hey this is a message")
-  console.log("REQ BODY: ", req.body)
-  console.log("MEDIA URL: ", req.body.MediaUrl)
-  var twiml = new twilio.TwimlResponse();
-    twiml.message(function() {
-      this.body('The Robots are coming! Head for the hills!');
-    });
-    res.writeHead(200, {'Content-Type': 'text/xml'})
-    res.end(twiml.toString())
+	console.log("REQ BODY: ", req.body)
+	console.log("MEDIA URL: ", req.body.MediaUrl0)
+	var twiml = new twilio.TwimlResponse();
+	twiml.message(function() {
+	  this.body('The Robots are coming! Head for the hills!');
+	});
+	res.writeHead(200, {'Content-Type': 'text/xml'})
+	res.end(twiml.toString())
 });
 
 module.exports = twilioAPI
