8b3a5bba9404562c566111145f0a7cee41c10c8a
rachael <hobbs.rachael@gmail.com>
2016-11-18 17:39:42 -0500

    added comments to delete script

--- liveconstants.js
+++ liveconstants.js
@@ -2,5 +2,12 @@ const accountSid = 'AC8b6aeccc3229dc3db665208f22c1e3c7'
 const authToken = 'bde747636cde1e8e5bf05e0b2c57c0c6'
 const twilioNum = '+12027593387'
 
+// Clarifai
+const clarifaiClientId = 'XZXCtUWl-ynABYs_Z2ipRZ6nEEj_MWhi5uFNYEh2'
+const clarifaiClientSecret = 'NwQSBuw8ON3MY04aDZ_Jl123zJAYkeo-cRYr4gZS'
+const clarifaiAccessToken = 'q9iyfJKOUxf5SSnjUquKKQBCAFMyJB'  //This will change every few days
 
-module.exports = {accountSid, authToken, twilioNum}
+
+
+
+module.exports = {accountSid, authToken, twilioNum, clarifaiClientId, clarifaiClientSecret, clarifaiAccessToken}
