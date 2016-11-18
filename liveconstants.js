2a13a49a445354e53cb3445777436756b8555fe1
rachael <hobbs.rachael@gmail.com>
2016-11-18 18:12:29 -0500

    cleaning up files

--- liveconstants.js
+++
@@ -1,6 +0,0 @@
-const accountSid = 'AC8b6aeccc3229dc3db665208f22c1e3c7'
-const authToken = 'bde747636cde1e8e5bf05e0b2c57c0c6'
-const twilioNum = '+12027593387'
-
-
-module.exports = {accountSid, authToken, twilioNum}
