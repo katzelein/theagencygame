9c50fba82264853f37a72f1cb32f8a32138261be
Kat <kathrynguthrie@gmail.com>
2016-11-18 18:31:41 -0500

    Update seed.js

--- models/seed.js
+++ models/seed.js
@@ -24,7 +24,7 @@ const data = {
     description: 'One of our agents disappeared somewhere into the depths of the Trump Building on Wall Street. We need you to investigate his disappearance, which may be ever more dire with the upcoming inauguration.'},
     { title: 'The Dark Underbelly of Broadway\'s Bright Lights', 
       description: 'Agent Natasha Klimikov was a rising star in the 1950s during Rodgers and Hammerstein\'s golden age. You\'ll be heading towards 42nd Street to retrace Natasha\'s steps and to see if her mission remains active.'},
-    { title: 'Grace Hopper and the Missing Dog', 
+    { title: 'Grace Hopper and the Missing Bone', 
       description: 'Ben, one of Grace Hopper Academy\'s proudest members, has had his favorite bone stolen out from under his nose. Can you identify the thief?'}
   ],
   challenge: [
