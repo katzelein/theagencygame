87966e73f70b2289dfa5ba6887ec5686e3332cef
Karin Kuo <evnissyn@gmail.com>
2016-11-17 20:10:26 -0500

    user

--- models/user.js
+++ models/user.js
@@ -5,7 +5,9 @@ const db = require('./_db')
 
 const User = db.define('users', {
   name: Sequelize.STRING,
-  phoneNumber: Sequelize.STRING
+  phoneNumber: Sequelize.STRING,
+  currentMission: Sequelize.INTEGER,
+  currentChallenge: Sequelize.INTEGER
 })
 
 module.exports = User
