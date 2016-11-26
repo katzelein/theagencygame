b9604e23b4a2e054a06a48dd83bce1dc1ce4bc86
Kat Guthrie <kathrynguthrie@gmail.com>
2016-11-26 16:04:13 -0500

    trying again to rebase the new master

--- browser/components/Dashboard.js
+++ browser/components/Dashboard.js
@@ -5,10 +5,9 @@ import Paper from 'material-ui/Paper';
 import {Table, TableBody, TableFooter, TableHeader, TableHeaderColumn, TableRow, TableRowColumn}
   from 'material-ui/Table';
 
-
 const styles = {
   paper: {
-    height: 500,
+    height: 100,
     width: 800,
     textAlign: 'center',
     display: 'inline-block',
@@ -21,13 +20,10 @@ const styles = {
   propToggleHeader: {
     margin: '20px auto 10px',
   },
+  table: {
+    height: 300
+  }
 };
-
-// Things to display:
-
-// List of Missions
-// Nested list of challenges
-// Data contained therein
 
 const tableData = [
   {
@@ -115,62 +111,61 @@ export default class Dashboard extends Component {
     console.log("DASHBOARD USER: ", this.props.user)
     return (
       <Paper style={styles.paper} zDepth={4} >
-        <Table height={'300'} >
-          <TableHeader >
-            <TableRow>
-              <TableHeaderColumn colSpan="3" tooltip="Dashboard" style={{textAlign: 'center'}}>
-                Dashboard
-              </TableHeaderColumn>
-            </TableRow>
-
-            <TableRow>
-              <TableHeaderColumn tooltip="The ID">ID</TableHeaderColumn>
-              <TableHeaderColumn tooltip="The Name">Name</TableHeaderColumn>
-              <TableHeaderColumn tooltip="The Status">Status</TableHeaderColumn>
-            </TableRow>
-
-          </TableHeader>
-
-          <TableBody
-            displayRowCheckbox={false}
-            deselectOnClickaway={true}
-            showRowHover={true} >
-
-            {tableData.map( (row, index) => (
-              <TableRow key={index} selected={row.selected}>
-                <TableRowColumn>{index}</TableRowColumn>
-                <TableRowColumn>{row.name}</TableRowColumn>
-                <TableRowColumn>{row.status}</TableRowColumn>
-              </TableRow>
-            ))}
-
-          </TableBody>
-
-          <TableFooter
-            adjustForCheckbox={this.state.showCheckboxes} >
-            
-            <TableRow>
-              <TableRowColumn>ID</TableRowColumn>
-              <TableRowColumn>Name</TableRowColumn>
-              <TableRowColumn>Status</TableRowColumn>
-            </TableRow>
-
-            <TableRow>
-              <TableRowColumn colSpan="3" style={{textAlign: 'center'}}>
-                Super Footer
-              </TableRowColumn>
-            </TableRow>
-
-          </TableFooter>
-
-        </Table>
-          <div> {this.props.user.username} </div>
-
           {this.props.user.id ? (
             <div>
               <div>
                   DASHBOARD
                   <div> {this.props.user.username} </div>
+                  <Table style={styles.table} >
+                    <TableHeader >
+                      <TableRow>
+                        <TableHeaderColumn colSpan="3" tooltip="Dashboard" style={{textAlign: 'center'}}>
+                          Dashboard
+                        </TableHeaderColumn>
+                      </TableRow>
+
+                      <TableRow>
+                        <TableHeaderColumn tooltip="The ID">ID</TableHeaderColumn>
+                        <TableHeaderColumn tooltip="The Name">Name</TableHeaderColumn>
+                        <TableHeaderColumn tooltip="The Status">Status</TableHeaderColumn>
+                      </TableRow>
+
+                    </TableHeader>
+
+                    <TableBody
+                      displayRowCheckbox={false}
+                      deselectOnClickaway={true}
+                      showRowHover={true} >
+
+                      {tableData.map( (row, index) => (
+                        <TableRow key={index} selected={row.selected}>
+                          <TableRowColumn>{index}</TableRowColumn>
+                          <TableRowColumn>{row.name}</TableRowColumn>
+                          <TableRowColumn>{row.status}</TableRowColumn>
+                        </TableRow>
+                      ))}
+
+                    </TableBody>
+
+                    <TableFooter
+                      adjustForCheckbox={this.state.showCheckboxes} >
+                      
+                      <TableRow>
+                        <TableRowColumn>ID</TableRowColumn>
+                        <TableRowColumn>Name</TableRowColumn>
+                        <TableRowColumn>Status</TableRowColumn>
+                      </TableRow>
+
+                      <TableRow>
+                        <TableRowColumn colSpan="3" style={{textAlign: 'center'}}>
+                          Super Footer
+                        </TableRowColumn>
+                      </TableRow>
+
+                    </TableFooter>
+
+                  </Table>
+
                   <div ><button type="button" onClick={this.logout}>Logout</button></div>
               </div>
 
