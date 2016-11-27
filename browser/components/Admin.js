f30a068000b833798fa21dc8551abf087eb59ae5
Ally Cody <allysoncody2015@u.northwestern.edu>
2016-11-27 11:27:08 -0500

    add challenge link to admin page

--- browser/components/Admin.js
+++ browser/components/Admin.js
@@ -13,7 +13,7 @@ export default class Admin extends Component {
          <div className="row">
               {/*this.props.user && this.props.user.isAdmin ? */
               (<div><div> THIS IS THE ADMIN PAGE </div>
-              <Link to="/admin/addMission"> Add Mission </Link>
+              <Link to="/admin/editChallenge"> View/Edit Challenges </Link>
               <Link to="/admin/editMission"> View/Edit Missions </Link>
               {this.props.children} </div>)
               /*:
