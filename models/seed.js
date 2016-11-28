58da845731978f03ff78798006cd6d9cb8b80c71
Karin Kuo <evnissyn@gmail.com>
2016-11-28 18:23:16 -0500

    shrink seed data

--- models/seed.js
+++ models/seed.js
@@ -27,6 +27,108 @@ const data = {
     }
   ],
 
+
+  mission: [
+    { title: 'Intrigue on Wall Street',  // mission 1
+    description: 'One of our agents'},
+    { title: 'The Dark Underbelly of Broadway\'s Bright Lights', // mission 2
+      description: 'Agent Natasha Klimikov'},
+    { title: 'Grace Hopper and the Missing Bone', // mission 3
+      description: 'Ben, one of Grace Hopper',
+      place: 'Grace Hopper',
+      numChallenges: 5
+    }
+  ],
+
+
+  challenge: [
+    { objective: 'Head to the Trump Building', // mission 1
+      summary: 'We need photographic evidence',
+      conclusion: 'Great work. The fingerprints'
+    },
+    { objective: 'Origins of the Open Market', // mission 1
+      summary: 'According to our surveillance', 
+      type: 'voice',
+      conclusion: 'Vinnie may be connected to the mob'
+    },
+
+    { objective: 'Find GHA\'s Newest Hero, Ceren', // mission 3
+      summary: 'Ceren, Ben\'s doting mom',
+      targetTags: ['bowl'],
+      conclusion: 'Great work. We\'re picking up' ,
+      order: 1
+    },
+    { objective: 'Putting Out Kitchen Fires', // mission 3
+      summary: 'Ben loves to wander',
+      conclusion: 'You\'re on the mark',
+      order: 2
+    },
+    { objective: 'Tracking the Teacher', // mission 3
+      summary: 'We have a list of all',
+      conclusion: 'Ashi may not be the',
+      order: 3
+    }, // imaginary friend-monster: gorp
+    { objective: 'Grace Hopper Academy\'s Secret Storage', // mission 3
+
+      summary: 'We think that the thief',
+      targetTags: ['gha_logo'],
+      conclusion: 'Our intel was correct',
+      order: 4
+    },
+    { objective: 'The Voice of Ultimate Betrayal', // mission 3
+      summary: 'This is where the rubber',
+      conclusion: 'We have a match.',
+      order:5
+    }
+  ],
+
+  userMission: [
+    {userId: 1, missionId: 1},
+    {userId: 2, missionId: 2},
+    {userId: 2, missionId: 3},
+    {userId: 3, missionId: 1}
+  ],
+
+  userChallenge: [
+    {userId: 1, challengeId: 1},
+    {userId: 1, challengeId: 2},
+    {userId: 2, challengeId: 1},
+    {userId: 2, challengeId: 2},
+    {userId: 2, challengeId: 3},
+    {userId: 2, challengeId: 4},
+    {userId: 2, challengeId: 5},
+    {userId: 2, challengeId: 6},
+    {userId: 2, challengeId: 7}
+  ]
+};
+
+db.sync({force: true})
+.then(() =>
+  User.bulkCreate(data.user))
+  .then(users => console.log(`Seeded ${users.length} users OK`))
+.then(() =>
+  Mission.bulkCreate(data.mission))
+  .then(missions => {
+    console.log(`Seeded ${missions.length} missions OK`)
+    return missions[2];
+  })
+  .then(mission => {
+    console.log(mission)
+    console.log('setChallenges', mission.setChallenges)
+    mission.setChallenges([3,4,5,6,7])
+  })
+.then(() =>
+  Challenge.bulkCreate(data.challenge))
+  .then(missions => console.log(`Seeded ${missions.length} challenges OK`))
+.then(() =>
+  UserMission.bulkCreate(data.userMission))
+  .then(userMissions => console.log(`Seeded ${userMissions.length} userMissions OK`))
+.then(() =>
+  UserChallenge.bulkCreate(data.userChallenge))
+  .then(userChallenges => console.log(`Seeded ${userChallenges.length} userChallenges OK`))
+
+
+/*
   mission: [
     { title: 'Intrigue on Wall Street',  // mission 1
     description: 'One of our agents disappeared somewhere into the depths of the Trump Building on Wall Street. We need you to investigate his disappearance, which may be ever more dire with the upcoming inauguration.'},
@@ -79,49 +181,5 @@ const data = {
       conclusion: 'We have a match. David Yang is the thief of the missing bone. It is a dark day for Grace Hopper, but a proud day for the Agency. Well done, agent. Your country, and Ben, thanks you.',
       order:5
     }
-  ],
-
-  userMission: [
-    {userId: 1, missionId: 1},
-    {userId: 2, missionId: 2},
-    {userId: 2, missionId: 3},
-    {userId: 3, missionId: 1}
-  ],
-
-  userChallenge: [
-    {userId: 1, challengeId: 1},
-    {userId: 1, challengeId: 2},
-    {userId: 2, challengeId: 1},
-    {userId: 2, challengeId: 2},
-    {userId: 2, challengeId: 3},
-    {userId: 2, challengeId: 4},
-    {userId: 2, challengeId: 5},
-    {userId: 2, challengeId: 6},
-    {userId: 2, challengeId: 7}
   ]
-};
-
-db.sync({force: true})
-.then(() =>
-  User.bulkCreate(data.user))
-  .then(users => console.log(`Seeded ${users.length} users OK`))
-.then(() =>
-  Mission.bulkCreate(data.mission))
-  .then(missions => {
-    console.log(`Seeded ${missions.length} missions OK`)
-    return missions[2];
-  })
-  .then(mission => {
-    console.log(mission)
-    console.log('setChallenges', mission.setChallenges)
-    mission.setChallenges([3,4,5,6,7])
-  })
-.then(() =>
-  Challenge.bulkCreate(data.challenge))
-  .then(missions => console.log(`Seeded ${missions.length} challenges OK`))
-.then(() =>
-  UserMission.bulkCreate(data.userMission))
-  .then(userMissions => console.log(`Seeded ${userMissions.length} userMissions OK`))
-.then(() =>
-  UserChallenge.bulkCreate(data.userChallenge))
-  .then(userChallenges => console.log(`Seeded ${userChallenges.length} userChallenges OK`))
+*/
