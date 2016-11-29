dc5382329e5590e21bbf91a9c040647ad74cb61d
rachael <hobbs.rachael@gmail.com>
2016-11-29 18:15:36 -0500

    keep having to change things to try them out on heroku

--- variables.js
+++ variables.js
@@ -4,7 +4,7 @@
 let localConst;
 
 if (process.env.DATABASE_URL) {
-  console.log('THE ENVIRONMENT IS ', process.env.NODE_ENV)
+  console.log('THE ENVIRONMENT IS ', process.env.DATABASE_URL)
   localConst = null
 } else {
   console.log('THE ENVIRONMENT IS ', process.env.NODE_ENV)
