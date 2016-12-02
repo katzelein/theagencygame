1d952cf750325cd9ef9b91dbdd982aa8e5741afa
rachael <hobbs.rachael@gmail.com>
2016-12-02 09:51:26 -0500

    restaging

--- routes/authy.js
+++ routes/authy.js
@@ -7,8 +7,12 @@ var User = require('../models/user');
 // router.get('/', function (req, res, next) {
 //   res.send("I'm working!")
 // })
+<<<<<<< HEAD
 
 var phoneReg = require('../phone_verification')(authyKey);
+=======
+var phoneReg = require('../phone_verification')(constants.authyKey);
+>>>>>>> master
 
 // https://github.com/seegno/authy-client
 const Client = require('authy-client').Client;
